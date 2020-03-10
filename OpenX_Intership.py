5d81e1254f1748f414a25d182c484802050b10ec
Patrick890 <patryk132253@gmail.com>
2020-03-10 22:49:20 +0100

    final version

--- OpenX_Intership.py
+++ OpenX_Intership.py
@@ -1,4 +1,5 @@
-import urllib.request, json, math
+import urllib.request, json
+from math import sqrt
 import numpy as np
 
 url1 = "https://jsonplaceholder.typicode.com/users"
@@ -10,59 +11,86 @@ postslist = json.loads(urllib.request.urlopen(url2).read())
 
 
 #1
-data = [dict() for number in range(len(postslist))]
+def data(users,posts):
+    data = [dict() for number in range(len(posts))]
 
-for i in range (0,len(postslist)):
-    for x,y in postslist[i].items():
-        data[i][x]=y
-    for j in range (0,len(userslist)):
-        if data[i]["userId"]==userslist[j]["id"]:
-            for x,y in userslist[j].items():
-                data[i][x]=y
-                data[i]["id"]=postslist[i]["id"]
+    for i in range (0,len(posts)):
+        for x,y in posts[i].items():
+            data[i][x]=y
+        for j in range (0,len(users)):
+            if data[i]["userId"]==users[j]["id"]:
+                for x,y in users[j].items():
+                    data[i][x]=y
+                    data[i]["id"]=posts[i]["id"]
 
+    return data
 #2
-for i in range (0,len(userslist)):
-    x=0
-    for j in range (0,len(postslist)):
-        if userslist[i]["id"]==postslist[j]["userId"]:
-            x+=1
-    print(userslist[i]["username"]," napisal(a) ",x," postow")
-
+def number_of_posts(users,posts):
+    postsnumber=[]
+    for i in range (0,len(users)):
+        x=0
+        for j in range (0,len(posts)):
+            if users[i]["id"]==posts[j]["userId"]:
+                x+=1
+        postsnumber.append(str(users[i]["username"])+' napisal(a) '+str(x)+ ' postow')
+    return postsnumber
 
 #3
-print("\n Lista tytulow ktore nie sa unikalne: \n")
+def unique_posts(posts):
+    ununique_post=[]
+    number_of_unique_posts=0
+    for i in range (0,len(posts)):
+        x=posts[i]["title"]
+        a=0
+        for j in range (0,len(posts)):
+            if j==i:
+                continue
+            if x==posts[j]["title"]:
+                a+=1
+        if a==0:
+            number_of_unique_posts+=1
+        else:
+            ununique_post.append(x)
+        
+    return [list(set(ununique_post)), number_of_unique_posts]
+   
 
-for i in range (0,len(postslist)):
-    x=postslist[i]["title"]
-    a=0
-    for j in range (0,len(postslist)):
-        if x==postslist[j]["title"]:
-            a+=1
-    if a==0:
-        print(x)
 
 #4
-print("\n Uzytkownicy mieszkajacy najblizej siebie:")
-
-for i in range (0,len(userslist)):
-    dist=[]
-    x=0
-    for j in range (0,len(userslist)):
-        if j==i:
-            continue
-        distance=math.sqrt((float(userslist[i]["address"]["geo"]["lat"])-float(userslist[j]["address"]["geo"]["lat"]))**2+(float(userslist[i]["address"]["geo"]["lng"])-float(userslist[j]["address"]["geo"]["lng"]))**2)
-        dist.append(distance)
-    index_min = np.argmin(dist)
-    if index_min>=i:
-        x=index_min+1
-    else:
-        x=index_min    
-        
-   # print(i,"   ",index_min,"  ")
-    print("Najblizej ",userslist[i]["name"]," ----> ",userslist[x]["name"],"  (",dist[index_min],")")
-            
-
+def users_distance(users):
+    users_dist=[]
+    for i in range (0,len(users)):
+        dist=[]
+        x=0
+        for j in range (0,len(users)):
+            if j==i:
+                continue
+            distance=sqrt((float(users[i]["address"]["geo"]["lat"])-float(users[j]["address"]["geo"]["lat"]))**2+(float(users[i]["address"]["geo"]["lng"])-float(users[j]["address"]["geo"]["lng"]))**2)
+            dist.append(distance)
+        index_min = np.argmin(dist)
+        if index_min>=i:
+            x=index_min+1
+        else:
+            x=index_min    
+           
+        users_dist.append("Najblizej "+str(users[i]["name"])+" ----> "+str(users[x]["name"])+"  ("+str(dist[index_min])+")")
+    return users_dist
 
 
+print(data(userslist,postslist)[34])
+print("\n\nIle postow napisali uzytkownicy:")
+for x in number_of_posts(userslist,postslist):
+    print(x)
+print("\n\nLiczba unikalnych tytulow: ",unique_posts(postslist)[1])
+print("\nLista tytulow ktore nie sa unikalne: \n",unique_posts(postslist)[0])
+print("\n\n Uzytkownicy mieszkajacy najblizej siebie:")
+for x in users_distance(userslist):
+    print(x)
+
+print("\n\n\n\n")
+
+posts = json.load(open("posts_test.txt"))
+users = json.load(open("users_test.txt"))
+for x in users_distance(users):
+    print(x)
 
---
+++ Test.py
@@ -0,0 +1,31 @@
+import unittest, json
+from OpenX_Intership import *
+
+posts = json.load(open("posts_test.txt"))
+users = json.load(open("users_test.txt"))
+
+
+class ProgramTest(unittest.TestCase):
+
+    
+
+    def test_data(self):
+        self.assertEqual(data(users,posts)[2],{'userId': 3, 'id': 3, 'title': 'tytul 3', 'name': 'Anna Graham', 'username': 'User 3', 'address': {'geo': {'lat': '-60', 'lng': '-50'}}})
+
+    def test_number_of_posts(self):
+        self.assertEqual(number_of_posts(users,posts)[2],"User 3 napisal(a) 2 postow")
+        self.assertEqual(number_of_posts(users,posts)[0],"Bret napisal(a) 1 postow")
+
+    def test_unique_posts(self):
+        self.assertEqual(unique_posts(posts),[['tytul 2'], 2])
+
+    def test_users_distance(self):
+        self.assertEqual(users_distance(users)[0],"Najblizej Patrick Graham ----> Ervin Howell  (110.45361017187261)")
+        self.assertEqual(users_distance(users)[1],"Najblizej Ervin Howell ----> Anna Graham  (28.284271247461902)")
+        self.assertEqual(users_distance(users)[2],"Najblizej Anna Graham ----> Ervin Howell  (28.284271247461902)")
+
+
+if __name__ == '__main__':
+    unittest.main()
+        
+
